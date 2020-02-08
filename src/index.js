77cf9181b66b2172a7116c38663e8378d223b866
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-02-08 16:03:06 +0900

    [server] DB 강제생성 제거

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ const { MODEL_NAMES } = require('./constants');
 (async () => {
   const userModel = sequelize.model(MODEL_NAMES.USER);
 
-  try {
-    await userModel.sync({ force: true });
-  } catch (error) {
-    console.log('Failed to create user table', error);
-  }
-
   const users = await userModel.findAll();
   users.forEach((user) => {
     const {
