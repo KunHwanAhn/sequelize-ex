d791effa2238ed027dbca9e5092811f5894dbed9
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-03-19 12:56:04 +0900

    [server] DB Config 정리

--- src/db/config/index.js
+++ src/db/config/index.js
@@ -1,61 +1,48 @@
 require('dotenv').config();
 
+const commonConfig = {
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  define: {
+    charset: 'utf8',
+    timestamps: true,
+    paranoid: true,
+  },
+  logging: (sql) => {
+    /* eslint-disable */
+    console.log(sql);
+    /* eslint-enable */
+  },
+};
+
 module.exports = {
   development: {
+    ...commonConfig,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      charset: 'utf8',
-      timestamps: true,
-      paranoid: true,
-    },
   },
   test: {
+    ...commonConfig,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: `${process.env.DB_NAME}_test`,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      charset: 'utf8',
-      timestamps: true,
-      paranoid: true,
-    },
   },
   production: {
+    ...commonConfig,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: `${process.env.DB_NAME}_production`,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      charset: 'utf8',
-      timestamps: true,
-      paranoid: true,
-    },
   },
 };
