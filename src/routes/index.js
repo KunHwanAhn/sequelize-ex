b0e4ba6d838a4152f0b08b39bd6c1ec6ac04295c
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-03-21 21:39:42 +0900

    [server] Add default router for '/'

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,11 @@ const api = require('./api');
 
 const router = new Router();
 
+router.get('/', ({ response }) => {
+  response.body = 'OK';
+});
+
 router.use('/api', api.routes());
+
 
 module.exports = router;
